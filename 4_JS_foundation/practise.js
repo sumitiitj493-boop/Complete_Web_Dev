84b61379f83393622928950670436ff5ed9ee04b
sumitiitj493-boop <sumitiitj493@gmail.com>
2026-01-09 23:32:27 +0530

    Web_Dev till now commited, all at once

---
+++ 4_JS_foundation/practise.js
@@ -0,0 +1,7 @@
+let europeanCities = ["Paris", "Rome"];
+let asianCities = ["Tokyo", "Bangkok"];
+
+// Merge arrays
+let worldCities = europeanCities.concat(asianCities);
+
+console.log(worldCities);
